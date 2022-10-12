75dd29a644a45baed361ad59205c21668fda4642
sem oktavianus <sem.oktavianus@gmail.com>
2022-10-12 20:23:58 +0700

    reverse string with test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let newString = "";
+  if (string) {
+    for (let i = string.length - 1; i >= 0; i--) {
+      newString += string[i];
+    }
+    return newString;
+  } else {
+    return "";
+  }
 };
 
 // Do not edit below this line
