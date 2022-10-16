ca587a5696946a8ea710c5000126e2af815f55d7
sem oktavianus <sem.oktavianus@gmail.com>
2022-10-16 23:05:07 +0700

    complete the task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (sum) {
+  if (sum.length < 1) {
+    return 0;
+  } else {
+    return sum.reduce((num1, num2) => {
+      return num1 + num2;
+    });
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (num) {
+  if (num.length < 1) {
+    return 0;
+  } else {
+    return num.reduce((num1, num2) => {
+      return num1 * num2;
+    });
+  }
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fac = 1;
+  for (let i = num; i > 0; i--) {
+    fac *= i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
